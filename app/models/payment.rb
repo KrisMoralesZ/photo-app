0b4f9d908ff8d2f4d2c84378ab24ddc282ef22a8
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-04-12 11:31:17 -0700

    process payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
-  attr_accessor :card_number
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
   belongs_to :user
 
   def self.month_options
@@ -13,7 +13,7 @@ class Payment < ApplicationRecord
   def process_payment
     customer = Stripe::Customer.create(email: email, card: token)
 
-    Stripe::Charge.create(customer: customer.id, amount: 10, description: 'Premium', currency: 'usd')
+    Stripe::Charge.create(customer: customer.id, amount: 1000, description: 'Premium', currency: 'usd')
   end
 
 end
