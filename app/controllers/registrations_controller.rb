c4f49d206ddf10418e2c8e199fc6243fd7e75d21
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-04-21 12:54:43 -0700

    add name and lastname fields in registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,46 +1,46 @@
 class RegistrationsController < Devise::RegistrationsController
-def create
-  build_resource(sign_up_params)
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  resource.class.transaction do
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      @payment = Payment.new({ email: params["user"]["email"], token: params[:payment]["token"], user_id: resource.id })
+  def create
+    build_resource(sign_up_params)
 
-      flash[:error] = "Please check registration errors" unless @payment.valid?
+    resource.class.transaction do
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        @payment = Payment.new({ email: params["user"]["email"], token: params[:payment]["token"], user_id: resource.id })
 
-      begin
-        @payment.process_payment
-        @payment.save
-      rescue Exception => e
-        flash[:error] = e.message
+        flash[:error] = "Please check registration errors" unless @payment.valid?
 
-        resource.destroy
-        puts 'Payment failed'
-        render :new and return
-      end
+        begin
+          @payment.process_payment
+          @payment.save
+        rescue Exception => e
+          flash[:error] = e.message
+          resource.destroy
+          render :new and return
+        end
 
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_flashing_format?
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
       end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
     end
   end
-end
 
-protected
+  protected
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up).push(:payment)
-end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname])
+  end
 end
