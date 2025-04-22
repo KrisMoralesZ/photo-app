7d2788b265b2053347aaae14fd83e0fbd0dd7d67
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-04-21 17:14:22 -0700

    Create Follows actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def following
+    @title = "Following"
+    @users = @user.following
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.followers
+  end
+
+  private
+  def set_user
+  end
 end
